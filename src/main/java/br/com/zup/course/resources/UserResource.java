dd1002c0932f73665ee98219530857c937b6cbe1
Gustavo Andrade <gustavoandrade15@hotmail.com>
2020-10-25 21:59:52 -0300

    User entity and resource

---
+++ src/main/java/br/com/zup/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.com.zup.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zup.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Gustavo", "gustavo@hotmail.com", "123456789", "2020");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
